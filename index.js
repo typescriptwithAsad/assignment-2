a3060988b8f83f06d88510ef10b564eda55715e0
asad <139746796+typescriptwithAsad@users.noreply.github.com>
2023-07-18 12:02:06 +0500

    Add files via upload
    my second butt uncomplete assignment.

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let personName = "Asad Farid";
+// step 1 :- show person name in lowercase
+let lowercaseName = personName.toLowerCase();
+//step 2 :- show person name in uppercase
+let uppercaseName = personName.toUpperCase();
+// step 3 :- show person name in titlecase  
+// i tried but i couldnot do that.
+console.log(lowercaseName);
+console.log(uppercaseName);
---
+++ index.ts
@@ -0,0 +1,17 @@
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let personName : string  = "Asad Farid";
+
+// step 1 :- show person name in lowercase
+
+let lowercaseName : string = personName.toLowerCase ()
+
+//step 2 :- show person name in uppercase
+
+let uppercaseName : string = personName.toUpperCase ();
+
+// step 3 :- show person name in titlecase  
+// i tried but i couldnot do that.
+
+console.log(lowercaseName);
+console.log(uppercaseName);
